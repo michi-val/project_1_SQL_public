ecc5f43a668ed45b5033b89e5ea161ee5e903fe1
Michael Faltynek <169072263+michi-val@users.noreply.github.com>
2024-09-24 13:54:19 +0200

    Update 1.4_prices_salary_trend.sql
    1) kapitálky, doplnění AS
    2) přidání sloupce "comparison_perc" změněno z ALTER TABLE na CAST přímo při vytváření tabulky

--- 1.4_prices_salary_trend.sql
+++ 1.4_prices_salary_trend.sql
@@ -6,15 +6,15 @@ WITH salary_price_year_comparison AS (
   SELECT *
   FROM
   	(SELECT 
-  		payroll_year payroll_year_a
-  		,round(avg(avg_salary),2) avg_salary_year 
-  	FROM t_michael_faltynek_project_sql_primary_final tmfpspf 
-  	GROUP BY  payroll_year_a) AS a_salary
+  		payroll_year AS payroll_year_a
+  		,ROUND(AVG(avg_salary),2) AS avg_salary_year 
+  	FROM t_michael_faltynek_project_sql_primary_final AS tmfpspf 
+  	GROUP BY payroll_year_a) AS a_salary
   JOIN 
   	(SELECT 
-  		payroll_year payroll_year_b
-  		,round(avg(price), 2) avg_price_year
-  	FROM t_michael_faltynek_project_sql_primary_final tmfpspf 
+  		payroll_year AS payroll_year_b
+  		,ROUND(AVG(price), 2) AS avg_price_year
+  	FROM t_michael_faltynek_project_sql_primary_final AS tmfpspf 
   	GROUP BY payroll_year_b) AS b_price
   ON a_salary.payroll_year_a = b_price.payroll_year_b)
 SELECT 
@@ -25,7 +25,7 @@ FROM salary_price_year_comparison;
 
 --
 SELECT *
-FROM v_salary_price_cz_yearly_1_4_help vspcyh 
+FROM v_salary_price_cz_yearly_1_4_help AS vspcyh 
 
 -- vytvoření tabulky s požadovanými daty procentuálního porování růstu cen a mezd
 CREATE OR REPLACE TABLE t_price_salary_comp_1_4_fin
@@ -33,25 +33,22 @@ WITH salary_price_1_4_help AS (
   SELECT 
   	payroll_year_a
   	,avg_salary_year 
-  	,lag(avg_salary_year) OVER (ORDER BY payroll_year_a) AS previous_value_sal	
+  	,LAG(avg_salary_year) OVER (ORDER BY payroll_year_a) AS previous_value_sal	
   	,avg_price_year
-  	,lag(avg_price_year) OVER (ORDER BY payroll_year_a) AS previous_value_pr	
+  	,LAG(avg_price_year) OVER (ORDER BY payroll_year_a) AS previous_value_pr	
   FROM v_salary_price_cz_yearly_1_4_help )
 SELECT 
-	payroll_year_a year
+	payroll_year_a  AS year
 	,avg_salary_year 
 	,avg_price_year
-	,round((((avg_salary_year - previous_value_sal) / previous_value_sal) *100) , 2) salary_comp_perc
-	,round((((avg_price_year - previous_value_pr) / previous_value_pr) *100) , 2) price_comp_perc
+	,ROUND((((avg_salary_year - previous_value_sal) / previous_value_sal) *100) , 2) AS salary_comp_perc
+	,ROUND((((avg_price_year - previous_value_pr) / previous_value_pr) *100) , 2) AS price_comp_perc
+	,CAST(NULL AS float) AS difference_perc		-- přidání sloupce "difference_perc" pro srovnání rozdílu růstu/poklesu cen a platů
 FROM salary_price_1_4_help;
 
--- přidání sloupce "difference_perc" pro srovnání rozdílu růstu/poklesu cen a platů
-ALTER TABLE t_price_salary_comp_1_4_fin
-ADD COLUMN difference_perc float;
-
--- naplnění sloupce daty 
+-- naplnění sloupce "difference_perc" daty 
 UPDATE t_price_salary_comp_1_4_fin
 SET difference_perc = price_comp_perc - salary_comp_perc; 
 
 SELECT *
-FROM t_price_salary_comp_1_4_fin tpscf;
+FROM t_price_salary_comp_1_4_fin AS tpscf;
