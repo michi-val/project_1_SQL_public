22e1d9da7ecf73e920d8bda8d205e08b8ae63b6a
Michael Faltynek <169072263+michi-val@users.noreply.github.com>
2024-11-05 13:40:28 +0100

    Update 1.3_commodity_price_trend.sql
    new version with a subselect

--- 1.3_commodity_price_trend.sql
+++ 1.3_commodity_price_trend.sql
@@ -1,46 +1,42 @@
 -- 3) Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
 
--- vytvoření tabulky pro extrakci dat z primary final + vytvoření sloupce "comparison" pro porovnání růstu/poklesu
-CREATE OR REPLACE TABLE t_value_comparison_avg_commodity_price_year
-WITH commodity_trend_help_data AS (	
+-- vytvoření tabulky pro extrakci dat z primary final + vytvoření sloupců "comparison" a "comparison_perc" pro porovnání růstu/poklesu
+
+CREATE OR REPLACE TABLE t_value_comparison_avg_commodity_price_year_new AS
+SELECT 
+	commodity_ss.payroll_year 
+	,commodity_ss.category_code
+	,commodity_ss.commodity_name 
+	,commodity_ss.ammount
+	,commodity_ss.unit
+	,commodity_ss.avg_price 
+	,commodity_ss.previous_value
+	,CASE
+		WHEN commodity_ss.avg_price > commodity_ss.previous_value THEN ('higher')
+		WHEN commodity_ss.avg_price < commodity_ss.previous_value THEN ('not higher')
+ 	END AS comparison
+ 	,ROUND((((commodity_ss.avg_price - commodity_ss.previous_value) / commodity_ss.previous_value) *100) , 2) AS comparison_perc
+FROM (
 	SELECT 
 		payroll_year 
 		,category_code 
 		,commodity_name 
-		,round(AVG(price), 2) AS avg_price 
+		,round(avg(price), 2) AS avg_price 
 		,ammount 
-		,unit  
+		,unit
+		,LAG(round(avg(price), 2)) OVER (PARTITION BY category_code ORDER BY payroll_year) AS previous_value
 	FROM t_michael_faltynek_project_sql_primary_final AS tmfpspf 
-	GROUP BY category_code, payroll_year)
-SELECT 
-	*
-	,LAG(avg_price) OVER (ORDER BY category_code, payroll_year) AS previous_value
-	,CASE
-		WHEN category_code != LAG(category_code) OVER (ORDER BY category_code, payroll_year) THEN  NULL 
-		WHEN LAG(category_code) OVER (ORDER BY category_code, payroll_year) IS NULL THEN NULL 
-    		WHEN avg_price > LAG(avg_price) OVER (ORDER BY category_code, payroll_year) THEN ('higher')
-    		ELSE 'not higher'
- 	END AS comparison
-	,CAST(NULL AS float) AS comparison_perc		-- přidání sloupce "comparison_perc" pro procentuální porovnání
-FROM commodity_trend_help_data;
-
--- naplnění comparison_perc
-UPDATE t_value_comparison_avg_commodity_price_year 
-SET comparison_perc = 
-	CASE 
-		WHEN comparison IS NULL THEN NULL 
-		ELSE ROUND((((avg_price - previous_value) / previous_value) *100) , 2)
-	END;
-
+	GROUP BY category_code, payroll_year, commodity_name, ammount) AS commodity_ss;
+	
 -- 
 SELECT *
-FROM t_value_comparison_avg_commodity_price_year AS tvcacpy;
+FROM t_value_comparison_avg_commodity_price_year_new AS tvcacpyn;
 
 -- průměrný meziroční nárůst jednotlivých produktů za celé období vzestupně
 SELECT 
 	commodity_name 
 	,ROUND(AVG(comparison_perc), 2) AS avg_price_trend
-FROM t_value_comparison_avg_commodity_price_year AS tvcacpy 
+FROM t_value_comparison_avg_commodity_price_year_new AS tvcacpyn
 GROUP BY commodity_name 
 ORDER BY avg_price_trend;
 
